ddb4dbf458bf343ab3f21be12e7b51846c6a1747
JuanCamilo268 <juleonba@unal.edu.co>
2024-11-08 18:54:31 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,7 +1,29 @@
+package test;
 class Auto{
     String modelo;
     int precio;
     Asiento[] asientos;
     String marca;
     Motor motor;
+    int registro;
+    static String cantidadCreados;
+    static int cantidadAsientos(Asiento[] asientos){
+        int cantidad = asientos.length();
+        return cantidad;
+    }
+    static void verificarIntegridad(Auto auto, Asiento[] asientos){
+        if (auto.registro != auto.motor.registro){
+            System.out.println("Las piezas no son originales");
+            break;
+        }
+        for (Asiento asiento: asientos){
+            if auto.registro != auto.asiento.registro{
+                System.out.println("Las piezas no son originales");
+                break;
+            }else{
+                System.out.println("Auto original")
+            }
+
+        }
+    }
 }
