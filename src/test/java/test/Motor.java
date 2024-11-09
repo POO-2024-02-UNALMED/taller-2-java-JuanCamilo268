6866b980536994f92a47886f06eb6e015e7313d2
JuanCamilo268 <juleonba@unal.edu.co>
2024-11-09 18:45:14 -0500

    Update Motor.java

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -3,4 +3,17 @@ class Motor{
     int numeroCilindros;
     String tipo;
     int registro;
+    static void cambiarRegistro(int reg){
+        this.registro = reg;
+    }
+    static void asignarTipo(String tipo){
+        Sring tip = tipo.toLowerCase();
+        if (tip == "electrico"){
+            this.tipo = tip;
+        }else if (tip == "gasolina"){
+            this.tipo = tip;
+        }else{
+            System.out.println("Ese tipo no existe")
+        }
+    }
 }
