65acdcb8a87f504b11d11b8aa26e47ce8d7941b9
JuanCamilo268 <juleonba@unal.edu.co>
2024-11-10 09:52:17 -0500

    Update Motor.java

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -3,10 +3,10 @@ class Motor{
     int numeroCilindros;
     String tipo;
     int registro;
-    static void cambiarRegistro(int reg){
+    void cambiarRegistro(int reg){
         this.registro = reg;
     }
-    static void asignarTipo(String tipo){
+    void asignarTipo(String tipo){
         String tip = tipo.toLowerCase();
         if (tip.equals("electrico")){
             this.tipo = tip;
